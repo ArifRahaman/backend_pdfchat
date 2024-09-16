09e7308fdaa81f92364dedc4712f0eb61d682cbc
Arif Rahaman <115584161+ArifRahaman@users.noreply.github.com>
2024-09-17 01:06:11 +0530

    Update generateAuthTokenAndSetCookie.js

--- utils/generateAuthTokenAndSetCookie.js
+++ utils/generateAuthTokenAndSetCookie.js
@@ -20,7 +20,7 @@ res.cookie("jwt", token, {
   httpOnly: true,
   secure: true, // Ensure HTTPS is used
   sameSite: 'None', // Required for cross-site cookies
-  domain: 'backend-pdfchatarif.onrender.com', // Update to match backend domain
+  Domain: 'frontend-pdfchat-2.onrender.com', // Update to match backend domain
 });
 
   
