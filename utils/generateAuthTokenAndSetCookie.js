a2f270ff3cd17acb8b2749fad9279a83457d304a
Arif Rahaman <115584161+ArifRahaman@users.noreply.github.com>
2024-09-17 16:06:14 +0530

    Update generateAuthTokenAndSetCookie.js

--- utils/generateAuthTokenAndSetCookie.js
+++ utils/generateAuthTokenAndSetCookie.js
@@ -17,7 +17,7 @@ const generateAuthTokenAndSetCookie = (user, res) => {
   
 res.cookie("jwt", token, {
   maxAge: 6 * 24 * 60 * 60 * 1000, // 6 days
-  httpOnly: true,
+  httpOnly: false,
   secure: true, // Ensure HTTPS is used
   sameSite: 'None', // Required for cross-site cookies
   Domain: 'frontend-pdfchat-2.onrender.com', // Update to match backend domain
